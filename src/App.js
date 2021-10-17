8de11cc11ef952eaa4716571835b31e7cf15d941
Alessandro Feitoza <alessandrofeitoza01@gmail.com>
2021-10-17 10:36:55 -0300

    Install bootstrap

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
     <div>
-      
+      <hr/>
+        <button className="btn btn-outline-danger">Inicio</button>
+        <button className="">Cadastro</button>
+        <button className="">Login</button>
+        <button className="">Dashboard</button>
+      <hr/>
 
       <Home/>
       <Register/>
